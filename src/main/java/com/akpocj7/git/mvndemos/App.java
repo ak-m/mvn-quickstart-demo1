dbf782430875abc54621e0e001acdfcd3cea2366
ak-m <ak.ghub.m@gmail.com>
2016-03-23 23:58:50 -0400

    initial commit of mvn quick start project

---
+++ src/main/java/com/akpocj7/git/mvndemos/App.java
@@ -0,0 +1,13 @@
+package com.akpocj7.git.mvndemos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
